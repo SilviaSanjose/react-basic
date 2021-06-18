0d26e1e1dabc9d3f31fb3a045e8a621c045ec80b
Silvia Sanjose <sanjose.silvia@gmail.com>
2021-06-18 09:55:22 +0200

    añadiendo sass

--- src/pages/Component.jsx
+++ src/pages/Component.jsx
@@ -25,7 +25,7 @@ function ComponentF() {
         de importar(según el método)
       </p>
 
-      <pre>{`
+      <pre className="component-pre">{`
          Component.jsx
             
             function ComponentF() {
