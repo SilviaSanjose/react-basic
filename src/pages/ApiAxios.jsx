d9092706e17787f27f40291a8ccbad077525be90
Silvia Sanjose <sanjose.silvia@gmail.com>
2021-06-24 17:19:02 +0200

    añado código al html

--- src/pages/ApiAxios.jsx
+++ src/pages/ApiAxios.jsx
@@ -28,27 +28,60 @@ const ApiAxios = () => {
       .then((res) => {
         //obtenemos la respuesta
         const response = res.data[0]; //guardo los datos que necesito en response (en este caso solo un usuario)
-        getUser(response); //añado el usuario al estado
-
-        //console.log(response);
+        getUser(response); //añado el usuario al estado;
       })
       .catch((error) => console.error(`Error: ${error}`));
   };
 
-  const getAll = async () => {
-    await axios
+  const getAll = () => {
+    axios
       .get(`${urlBase}posts`) //llamamos a la url
       .then((res) => {
         //obtenemos la respuesta
         const response = res.data; //guardo los datos que necesito en response (en este caso solo un usuario)
         getUser(response); //añado el usuario al estado
-
-        //console.log(response);
       })
       .catch((error) => console.error(`Error: ${error}`));
   };
 
-  return <ApiRes data={user} apicall={apicall} />;
+  return (
+    <div>
+      <h2>LLamada Api con Axios</h2>
+      <pre>{`
+      const [user, getUser] = useState(""); 
+      const apicall = "getAll";   //  const apicall = "getOne";
+    
+      useEffect(() => {
+        if (apicall === "getAll") {
+          getAll();
+        } else if (apicall === "getOne") {
+          getOne();
+        }
+      }, []);
+    
+      const getOne = () => {
+        axios
+          .get(https://jsonplaceholder.typicode.com/posts ') 
+          .then((res) => {
+            const response = res.data[0]; 
+            getUser(response); 
+          })
+          .catch((error) => console.error('Error'));
+      };
+    
+      const getAll = () => {
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts') 
+          .then((res) => {
+            const response = res.data; 
+            getUser(response); 
+          })
+          .catch((error) => console.error('Error'));
+      };
+      `}</pre>
+      <ApiRes data={user} apicall={apicall} />
+    </div>
+  );
 };
 
 export default ApiAxios;
