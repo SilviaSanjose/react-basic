eaa6795e847163f393386bcc3f599e6d105fde11
Silvia Sanjose <sanjose.silvia@gmail.com>
2021-07-05 16:49:56 +0200

    formularios

---
+++ src/components/Formularios.jsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+
+const Formularios = () => {
+  //formularios
+  const [name, setName] = useState("");
+  const handlerName = (e) => {
+    //ejemplo al cambiar input, el resto se hace en el mismo input
+    setName(e.target.value);
+  };
+  const [sabor, setSabor] = useState("");
+  const [lenguage, setlenguage] = useState("");
+  const [terms, setTerms] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Nombre: ${name} 
+    Seleccionado ${sabor} y ${lenguage}
+    Condiciones: ${terms}`);
+  };
+  //formulario 2
+  const [form, setForm] = useState({});
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleChecked = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.checked,
+    });
+  };
+  const handleSubmit2 = (e) => {
+    e.preventDefault();
+    alert(`Nombre: ${form.nom} 
+    Condiciones: ${form.terminos}`);
+  };
+  return (
+    <>
+      <h2>Formularios</h2>
+      {/*formulario recogiendo datos por separado*/}
+      <div>
+        Para ser controlados, la tag form no debe llevar método, si no un{" "}
+        <b>onSubmit </b> que desactive la funcionalidad por defecto del submit.
+      </div>
+      <code>
+        <pre>{`
+        const handleSubmit = (e) => {
+            e.preventDefault();
+          };
+          
+        <form onSubmit={handleSubmit}></form>`}</pre>{" "}
+      </code>
+      <p>Formulario recogiendo los datos individualmente.</p>
+      <div className="d-flex">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor={"nombre"} className="mr-2">
+            Nombre:{" "}
+          </label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={name}
+            onChange={handlerName}
+          />
+          <br />
+          <label className="mr-2">Seleccionado "{sabor}". Cambiar: </label>
+          <label className="mr-2">Vanilla</label>
+          <input
+            type="radio"
+            name="sabor"
+            value="vanilla"
+            onChange={(e) => setSabor(e.target.value)}
+          />
+          <label className="m-2">React</label>
+          <input
+            type="radio"
+            name="sabor"
+            value="react"
+            onChange={(e) => setSabor(e.target.value)}
+            defaultChecked
+          />
+          <br />
+          <label className="mr-2">Seleccionado "{lenguage}":</label>
+          <select
+            name="lenguaje"
+            selected
+            onChange={(e) => setlenguage(e.target.value)}
+            defaultValue="ninguno"
+          >
+            <option value="ninguno">Selecciona</option>
+            <option value="js">JavaScript</option>
+            <option value="python">Python</option>
+          </select>
+          <br />
+          <label className="mr-2">Acepto</label>
+          <input
+            type="checkbox"
+            name="terminos"
+            onChange={(e) => setTerms(e.target.checked)}
+          />
+          {terms ? " Aceptado" : " Rechazado"}
+          <br />
+          <input type="submit" value="Enviar" />
+        </form>
+        <div>
+          Cada input debe llevar su <b>onChange</b> que llama a una función por
+          cada campo que añade al estado el valor del input. Tantas funciones y
+          <b> setState </b>como campos tenga el formulario.
+          <ul>
+            <li>
+              Input radio, select, text, textare...Se recoge el value:
+              <br />
+              <code>onChange=|(e) =&gt; setSabor(e.target.value)|</code>
+            </li>
+            <li>
+              Checkbox se recoge el checked: <br />
+              <code>onChange=|(e) =&gt; setTerms(e.target.checked)|</code>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <p>Formulario recogiendo los datos en grupo.</p>
+      <p>
+        En este caso, todos los campos del formulario llaman a la misma función.
+        Sólo se separan en funciones distintas los campos que recogen un valor y
+        los checkox que recogen el checked
+      </p>
+      <div className="d-flex">
+        <div>
+          <form onSubmit={handleSubmit2} className="form">
+            <input type="text" id="nombre" name="nom" onChange={handleChange} />
+            <br />
+            <label className="mr-2">Acepto:</label>
+            <input type="checkbox" name="terminos" onChange={handleChecked} />
+            <br />
+            <input type="submit" value="Enviar" />
+          </form>
+          <ul>
+            <li>
+              text, select, ect.. <code> onChange=|handleChange|</code>
+            </li>
+            <li>
+              checkbox: <code> onChange=|handleChecked|</code>
+            </li>
+          </ul>
+        </div>
+        <pre>{`
+            const handleChange = (e) => { 
+                setForm({
+                     ...form, [e.target.name]:e.target.value 
+                }); }; 
+            const handleChecked = (e) => { 
+                setForm({
+                     ...form, 
+                     [e.target.name]: e.target.checked
+                }); };
+            `}</pre>
+      </div>
+    </>
+  );
+};
+
+export default Formularios;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,48 @@
 import Renderizado from "../components/Renderizado";
 import { useState } from "react";
+import Formularios from "../components/Formularios";
 
 const Home = () => {
-    
-    let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"]
-  
-    const [render, changeRender] =  useState(false);
-    const ocultar =()=> changeRender(false);
-    
-    return (
-        
-        <main>
-            <div>
+  let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"];
 
-                <p>Para recorrer arrays, se usa el método <b>.map()</b>: </p>
-                <div className="d-flex justify-content-around">
-                    <ul>
-                    {estaciones.map((el, index) => <li key={index}>{el}</li>)}
-                    </ul>
-                    <pre className="pr-4">{`
+  //renderizado
+  const [render, changeRender] = useState(false);
+  const ocultar = () => changeRender(false);
+
+  return (
+    <main>
+      <div>
+        <p>
+          Para recorrer arrays, se usa el método <b>.map()</b>:{" "}
+        </p>
+        <div className="d-flex justify-content-around">
+          <ul>
+            {estaciones.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+          <pre className="pr-4">{`
             let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"]
             <ul>
             {estaciones.map((el, index) => <li key={index}>{el}</li>)}
             </ul>
                     `}</pre>
-                </div>
-            </div>
-        <section>
-        <section>
-            <h2>Renderizado</h2>
-            <p>Se va a mostrar un componente sólo si la variable <b>render</b> es true. Para poder renderizar un componente según haya cambios se 
-            necesita controlar su estado, con el Hook <b>useSate()</b> el cual debe recibir un valor inicial, y ha de instanciarse con destructuración 
-            de arreglos con dos elementos: estado inicial y método para cambiarlo: </p>
-            <button onClick={()=> changeRender(true)}>Renderizar</button>
-            <button onClick={ocultar}>Ocultar</button>
-            {render ? <Renderizado />: "  Pulsa para renderizar el componente"}
-            <pre className="my-2">{`
+        </div>
+      </div>
+      <section>
+        <h2>Renderizado</h2>
+        <p>
+          Se va a mostrar un componente sólo si la variable <b>render</b> es
+          true. Para poder renderizar un componente según haya cambios se
+          necesita controlar su estado, con el Hook <b>useSate()</b> el cual
+          debe recibir un valor inicial, y ha de instanciarse con
+          destructuración de arreglos con dos elementos: estado inicial y método
+          para cambiarlo:{" "}
+        </p>
+        <button onClick={() => changeRender(true)}>Renderizar</button>
+        <button onClick={ocultar}>Ocultar</button>
+        {render ? <Renderizado /> : "  Pulsa para renderizar el componente"}
+        <pre className="my-2">{`
             import { useState } from "react";
             const [render, changeRender] =  useState(false);
             const ocultar =()=> changeRender(false);
@@ -43,16 +50,15 @@ const Home = () => {
             <button onClick={()=> changeRender(true)}>Renderizar</button>
             <button onClick={ocultar}>Ocultar</button>
             {render ? <Renderizado />: "  Pulsa para renderizar el componente"}
-
+            
             `}</pre>
-        </section>
-          
-        </section>
+      </section>
+      <br />
+      <section>
+        <Formularios />
+      </section>
+    </main>
+  );
+};
 
-        
-      </main>
-        
-    )
-}
-
-export default Home
+export default Home;
